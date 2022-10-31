9b099514ee70d54e02bb5090c678cb21d24334cc
Sama's PC <Sama's PC@DESKTOP-ML5L0QU>
2022-10-31 10:43:28 +0200

    adding the editing profle feature

--- src/test/java/testCode/EditCustomerInfo.java
+++ src/test/java/testCode/EditCustomerInfo.java
@@ -10,6 +10,7 @@ import mainCode.*;
 
 public class EditCustomerInfo {
 static int passF=0;
+String correctPass="saba2007";
 
 Customer customer= new Customer();
 Customer customer1 = new Customer ("1", "saba", "saba2007","saba2007@gmail.com",598667006);
@@ -41,9 +42,9 @@ List <Customer> customers = new ArrayList <Customer>();
 	@When("user enters right password and wants to edit the email")
 	public void userEntersRightPasswordAndWantsToEditTheEmail() {
 		main.addCustomer(customer1);
-		 String Pass="saba2007";
+		 //String Pass="saba2007";
 	    String newEmail="sabafadda@gmail.com";
-	    if(Customer.ValidPassword(Pass, main)) {
+	    if(Customer.ValidPassword(correctPass, main)) {
 	    	customer.setEmail(newEmail);
 	    	customers.add(customer);
 	    	main.setCustomers(customers);
@@ -62,25 +63,49 @@ List <Customer> customers = new ArrayList <Customer>();
 
 	@When("user enters right password and wants to edit the password")
 	public void userEntersRightPasswordAndWantsToEditThePassword() {
-	 
+		main.addCustomer(customer1);
+		 //String Pass="saba1234";
+	    String newPassword="sabafadda@gmail.com";
+	    if(Customer.ValidPassword(correctPass, main)) {
+	    	customer.setPassword(newPassword);
+	    	customers.add(customer);
+	    	main.setCustomers(customers);
+	    	
+			   passF=0;
+		   }
 	}
 	@Then("the customer should see the password has been edited")
 	public void theCustomerShouldSeeThePasswordHasBeenEdited() {
-
+		 if(passF==0) {
+			  assertTrue(passF==0);
+			  System.out.println("the Password has been edited");
+		  }
 	}
 
 	
 
 	@When("user enters right password and wants to edit the phone number")
 	public void userEntersRightPasswordAndWantsToEditThePhoneNumber() {
-	
+		main.addCustomer(customer1);
+		// String Pass="saba1234";
+	    long newPhone=59867265;
+	    if(Customer.ValidPassword(correctPass, main)) {
+	    	customer.setPhoneNumber(newPhone);
+	    	customers.add(customer);
+	    	main.setCustomers(customers);
+	    	
+			   passF=0;
+	}
 	}
 	@Then("the customer should see the phone number has been edited")
 	public void theCustomerShouldSeeThePhoneNumberHasBeenEdited() {
-	 
+		 if(passF==0) {
+			  assertTrue(passF==0);
+			  System.out.println("the phone number has been edited");
+		  
 	}
 
-
+	}
 
 
 
